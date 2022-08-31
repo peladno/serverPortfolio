061f62875158fdf7edda86160722fb31710692e3
Javier Perez <j.perezurrutia@gmail.com>
2022-08-31 03:08:21 -0400

    last test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const httpServer = http.createServer(app);
 
 app.use(cors({
-  "origin": `${clientURL}`,
+  "origin": "*",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204
